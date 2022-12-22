3b899c869ae0aac7f8617db3e9d96ec49e721660
erico <ecocaval@hotmail.com>
2022-12-22 00:26:16 -0300

    adding complete calendar and subtitles to records page

--- src/pages/Records.js
+++ src/pages/Records.js
@@ -1,23 +1,55 @@
+import { useEffect, useState } from "react"
+
 // libraries
 import styled from "styled-components"
+import dayjs from "dayjs"
 
 // components
 import UserHeader from "../components/UserHeader"
 import UserMenu from "../components/UserMenu"
-
 import Calendar from 'react-calendar'
 import 'react-calendar/dist/Calendar.css';
+import axios from "axios"
+
 
 export default function Record() {
 
-    return(
+    const [date, setDate] = useState(new Date());
+    const [recordsInfo, setRecordsInfo] = useState([])
+
+    console.log(recordsInfo);
+
+    const config = {
+        headers: {
+            "Authorization": "Bearer " + JSON.parse(localStorage.getItem('userInfo')).token
+        }
+    }
+
+    useEffect(() => {
+        const recordsUrl = "https://mock-api.bootcamp.respondeai.com.br/api/v2/trackit/habits/history/daily"
+
+        axios.get(recordsUrl, config)
+            .then(res => {
+                setRecordsInfo(res.data)
+            })
+            .catch(err => console.log(err))
+
+
+    }, [])
+
+    return (
         <RecordWrapper>
-            <UserHeader/>
-                <RecordHeader>
-                    <h2>Histórico</h2>
-                    <Calendar/>
-                </RecordHeader>
-            <UserMenu/>
+            <UserHeader />
+            <RecordHeader>
+                <h2>Histórico</h2>
+                <Calendar
+                    onChange={setDate}
+                    value={date}
+                    locale="pt-BR"
+                    maxDate={new Date()}
+                    formatDay={(_, date) => dayjs(date).format('D')} />
+            </RecordHeader>
+            <UserMenu />
         </RecordWrapper>
     )
 }
@@ -39,10 +71,5 @@ const RecordHeader = styled.div`
         color: #126BA5;
         margin: auto 0px;
         margin-bottom: 20px;
-    }
-    > p {
-        font-family: 'Lexend Deca';
-        font-size: 18px;
-        color: #666666;
     }
 `
